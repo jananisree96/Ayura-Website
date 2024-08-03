15727cb703e3ad1950f898ea61ade53fa5928046
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2024-08-03 11:18:51 +0530

    navbar

---
+++ final-project/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      <h1>My Website</h1>
+    </div>
+  )
+}
+
+export default NavBar
