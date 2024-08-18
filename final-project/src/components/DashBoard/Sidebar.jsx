a1a16011ab5157b03dbb14aa83e8d8462ed39a41
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2024-08-18 20:45:21 +0530

    sidebar-bgcolor-removed

--- final-project/src/components/DashBoard/Sidebar.jsx
+++ final-project/src/components/DashBoard/Sidebar.jsx
@@ -15,43 +15,36 @@ const Sidebar = () => {
       mode="inline"
       theme="light"
       defaultSelectedKeys={['dashboard']}
-      style={{ height: '100vh',width:'200px', borderRight: 0,cursor:'pointer',
-        backgroundColor: '#9CEC7C'}}
+      style={{ height: '100vh', width: '200px', borderRight: 0, cursor: 'pointer'}}
     >
       <Menu.Item key="dashboard" icon={<DashboardOutlined />}
-      style={{ color: '#000' }}
-      onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#b8f5a9')}
-      onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}>
+        style={{ color: '#000' }}
+      >
         Overview
       </Menu.Item>
       <Menu.Item key="profile" icon={<UserOutlined />}
-      style={{ color: '#000' }}
-      onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#b8f5a9')}
-      onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}>
+        style={{ color: '#000' }}
+      >
         Customers
       </Menu.Item>
       <Menu.Item key="analytics" icon={<BarChartOutlined />}
-      style={{ color: '#000' }}
-      onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#b8f5a9')}
-      onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}>
+        style={{ color: '#000' }}
+      >
         Analytics
       </Menu.Item>
       <Menu.Item key="notifications" icon={<BellOutlined />}
-      style={{ color: '#000' }}
-      onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#b8f5a9')}
-      onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}>
+        style={{ color: '#000' }}
+      >
         Notifications
       </Menu.Item>
       <Menu.Item key="settings" icon={<SettingOutlined />}
-      style={{ color: '#000' }}
-      onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#b8f5a9')}
-      onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}>
+        style={{ color: '#000' }}
+      >
         Settings
       </Menu.Item>
       <Menu.Item key="logout" icon={<LogoutOutlined />}
-      style={{ color: '#000' }}
-      onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#b8f5a9')}
-      onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}>
+        style={{ color: '#000' }}
+      >
         Logout
       </Menu.Item>
     </Menu>
